27e508948a0dd20eb10e9e875f2e18a09e860907
disappear <mrd791204@gmail.com>
2024-11-05 16:27:05 +0800

    Initial Commit

---
+++ warChallengeAppOriginal/warChallengeAppOriginalApp.swift
@@ -0,0 +1,17 @@
+//
+//  warChallengeAppOriginalApp.swift
+//  warChallengeAppOriginal
+//
+//  Created by billHsiao on 2024/11/5.
+//
+
+import SwiftUI
+
+@main
+struct warChallengeAppOriginalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ warChallengeAppOriginalTests/warChallengeAppOriginalTests.swift
@@ -0,0 +1,17 @@
+//
+//  warChallengeAppOriginalTests.swift
+//  warChallengeAppOriginalTests
+//
+//  Created by billHsiao on 2024/11/5.
+//
+
+import Testing
+@testable import warChallengeAppOriginal
+
+struct warChallengeAppOriginalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
